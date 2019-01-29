992ea8b73e28c831446c9b578fd881e8b547c793
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-01-29 22:17:35 +0700

    test3 update

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ require "vendor/autoload.php";
 
 $access_token = 'nbNiHyLgU5prZPC7JvvpNZgnX2zGYIihVT8tA4vQrdH1sILvxpfPjzM4YpBauEQkeIXVLtCUm1OY0yfvA6TtU+1sS8sp/+kdmSjZWENUDMWMq8vAkT0PJxsqYJdxtEqzsnvNfLTyCX45iVXfVV8lgFGUYhWQfeY8sLGRXgo3xvw=';
 
-$channelSecret = '520a623a108c62d2fff474b028d343e5';
+$channelSecret = 'cb793858eacb4e1fa813e3d98334bce8';
 $idPush = 'U3f2bf07d27dc888a5ac554fa42b99f20'
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
@@ -12,3 +12,4 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('à¸£à¸
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+?>
