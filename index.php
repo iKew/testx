3c4757a0ceca8fe5e67e31704cba259acd29e628
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-01-29 22:34:51 +0700

    test3 update

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require "vendor/autoload.php";
 $access_token = 'nbNiHyLgU5prZPC7JvvpNZgnX2zGYIihVT8tA4vQrdH1sILvxpfPjzM4YpBauEQkeIXVLtCUm1OY0yfvA6TtU+1sS8sp/+kdmSjZWENUDMWMq8vAkT0PJxsqYJdxtEqzsnvNfLTyCX45iVXfVV8lgFGUYhWQfeY8sLGRXgo3xvw=';
 
 $channelSecret = 'cb793858eacb4e1fa813e3d98334bce8';
-$idPush = 'U3f2bf07d27dc888a5ac554fa42b99f20';
+$idPush = 'U3a23ef7fc7a6bff7cb7400d5f829b5fe"';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('รักอายน้าาาาา :3');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ควยยยยยยย ไอโฟร์คคคคคคคค');
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
