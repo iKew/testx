418128d0f6525b8f97efeb432acca31c830a24bf
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-01-29 22:36:29 +0700

    test3 update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require "vendor/autoload.php";
 $access_token = 'nbNiHyLgU5prZPC7JvvpNZgnX2zGYIihVT8tA4vQrdH1sILvxpfPjzM4YpBauEQkeIXVLtCUm1OY0yfvA6TtU+1sS8sp/+kdmSjZWENUDMWMq8vAkT0PJxsqYJdxtEqzsnvNfLTyCX45iVXfVV8lgFGUYhWQfeY8sLGRXgo3xvw=';
 
 $channelSecret = 'cb793858eacb4e1fa813e3d98334bce8';
-$idPush = 'U3a23ef7fc7a6bff7cb7400d5f829b5fe"';
+$idPush = 'U3a23ef7fc7a6bff7cb7400d5f829b5fe';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ควยยยยยยย ไอโฟร์คคคคคคคค');
