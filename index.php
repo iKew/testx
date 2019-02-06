14fafe1a0f7fe13f43c2d37c863235b7169b02ca
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-02-06 22:52:29 +0700

    edit text

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $channelSecret = 'cb793858eacb4e1fa813e3d98334bce8';
 $idPush = 'U3a23ef7fc7a6bff7cb7400d5f829b5fe';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ควยยยยยยย ไอโฟร์คคคคคคคค');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('กูแก้ให้ละ');
 $response = $bot->pushMessage($idPush, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
