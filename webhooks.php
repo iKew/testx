a951e402f801804326686b2c89db7f501d1c5a1e
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-02-07 23:32:12 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -11,8 +11,39 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['originalDetectIntentRequest'])) {
-	
+	echo "ok";
 	// Loop through each event
+	$userId = $request['originalDetectIntentRequest']['payload']['data']['source']['userId'];
+
+	$text = $userId;
+	// 		// Get replyToken
+			$replyToken = $event['responseId'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 	// foreach ($events['events'] as $event) {
 	// 	// Reply only when message sent is in 'text' format
 	// 	if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
@@ -48,12 +79,6 @@ if (!is_null($events['originalDetectIntentRequest'])) {
 	// 		echo $result . "\r\n";
 	// 	}
 	// }
-	// 
-	$userId = $request[‘originalDetectIntentRequest’][‘payload’][‘data’][‘source’][‘userId’];
-	$myfile = fopen(“log$date.txt”, “a”) or die(“Unable to open file!”);
-	$log = $date.”-”.$time.”\t”.$userId.”\t”.$queryText.”\n”;
-	fwrite($myfile,$log);
-	fclose($myfile);
 }
 
-echo "ok";
+
