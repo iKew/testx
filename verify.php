044545980e5f485b0c48cfb658c4dc2c287eb852
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-02-09 23:53:42 +0700

    test

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = 'nbNiHyLgU5prZPC7JvvpNZgnX2zGYIihVT8tA4vQrdH1sILvxpfPjzM4YpBauEQkeIXVLtCUm1OY0yfvA6TtU+1sS8sp/+kdmSjZWENUDMWMq8vAkT0PJxsqYJdxtEqzsnvNfLTyCX45iVXfVV8lgFGUYhWQfeY8sLGRXgo3xvw=';
 
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
