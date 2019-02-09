afe28b2d363eb2a12deba3f29fc3c30ad0f7aac3
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-02-09 23:10:51 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -9,7 +9,7 @@ $access_token = 'nbNiHyLgU5prZPC7JvvpNZgnX2zGYIihVT8tA4vQrdH1sILvxpfPjzM4YpBauEQ
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 //$userId = $events['originalDetectIntentRequest']['payload']['data']['source']['userId'];
-echo $events;
+echo $events[0]['responseId'];
 
 echo "\n";
 echo $content;
