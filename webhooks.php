97e65a2f3279e926e10a92bd89a059f26d539805
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2019-02-09 23:07:24 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -10,33 +10,33 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 //$userId = $events['originalDetectIntentRequest']['payload']['data']['source']['userId'];
 echo $events;
-$replyToken = $events['responseId'];
+// $replyToken = $events['responseId'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'dddd'
-			];
+// 			// Build message to reply back
+// 			$messages = [
+// 				'type' => 'text',
+// 				'text' => 'dddd'
+// 			];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken ,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// 			// Make a POST Request to Messaging API to reply to sender
+// 			$url = 'https://api.line.me/v2/bot/message/reply';
+// 			$data = [
+// 				'replyToken' => $replyToken ,
+// 				'messages' => [$messages],
+// 			];
+// 			$post = json_encode($data);
+// 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+// 			$ch = curl_init($url);
+// 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+// 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// 			$result = curl_exec($ch);
+// 			curl_close($ch);
 
-			echo $result . "\r\n";
+// 			echo $result . "\r\n";
 
 
 
